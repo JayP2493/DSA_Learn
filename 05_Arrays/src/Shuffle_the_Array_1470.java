ab3e2db9bf0cf4d30d4b876fa98ff86a15306c7c
jay1patel1 <jayp.fsd@outlook.com>
2024-09-01 21:12:04 +0530

    updated

---
+++ 05_Arrays/src/Shuffle_the_Array_1470.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Shuffle_the_Array_1470 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(shuffle(new int[] {2,5,1,3,4,7},3)));
+        System.out.println(Arrays.toString(shuffle(new int[] {1,2,3,4,4,3,2,1},4)));
+        System.out.println(Arrays.toString(shuffle(new int[] {1,1,2,21},1)));
+    }
+    public static int[] shuffle(int[] nums, int n) {
+
+    }
+}
