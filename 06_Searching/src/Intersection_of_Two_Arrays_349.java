f59607fb60deebba647800f820004ead7a30a712
jay1patel1 <jayp.fsd@outlook.com>
2024-09-15 16:32:09 +0530

    349 java done

--- 06_Searching/src/Intersection_of_Two_Arrays_349.java
+++ 06_Searching/src/Intersection_of_Two_Arrays_349.java
@@ -1,7 +1,14 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Intersection_of_Two_Arrays_349 {
     public static int[] intersection(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            return intersection (nums2,nums1);
+        }
+
+        Arrays.sort(nums1);
         int[] arr1 = new int[1010];
         List<Integer> ans = new ArrayList();
 
@@ -9,6 +16,7 @@ public class Intersection_of_Two_Arrays_349 {
             if (arr1[nums2[i]] > 0) {
                 continue;
             } else {
+                arr1[nums2[i]]++;
                 if (find(nums1,nums2[i])) {
                     ans.add(nums2[i]);
                 } else {
@@ -17,10 +25,10 @@ public class Intersection_of_Two_Arrays_349 {
             }
         }
 
-        int[] ansArr = new int[ans.size()-1];
+        int[] ansArr = new int[ans.size()];
 
         for(int i = 0; i < ans.size(); i++) {
-            ansArr[i] = ans.get(i); 
+            ansArr[i] = ans.get(i);
         }
 
         return ansArr;
@@ -30,7 +38,7 @@ public class Intersection_of_Two_Arrays_349 {
         int start = 0;
         int end = arr.length-1;
 
-        while ( start < end ) {
+        while ( start <= end ) {
             int mid = start + (end-start)/2;
 
             if (arr[mid] == target) {
@@ -47,5 +55,6 @@ public class Intersection_of_Two_Arrays_349 {
 
     public static void main(String[] args) {
         System.out.println(Arrays.toString(intersection(new int[] {1,2,2,1}, new int[] {2,2})));
+        System.out.println(Arrays.toString(intersection(new int[] {4,9,5}, new int[] {9,4,9,8,4})));
     }
 }
