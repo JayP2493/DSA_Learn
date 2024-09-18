7f17f18b50e43c1aa66d093c2b31ebc7fab0250a
jay1patel1 <jayp.fsd@outlook.com>
2024-09-18 22:40:44 +0530

    1608 done

--- 06_Searching/src/Special_Array_With_X_Elements_Greater_Than_Or_Equal_X_1608.java
+++ 06_Searching/src/Special_Array_With_X_Elements_Greater_Than_Or_Equal_X_1608.java
@@ -4,20 +4,13 @@ public class Special_Array_With_X_Elements_Greater_Than_Or_Equal_X_1608 {
     public static int specialArray(int[] nums) {
         Arrays.sort(nums);
 
-        int start = 1;
-        int end = nums.length;
-
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            if (countBigger(nums,mid) > mid) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
+        for (int i = 1; i <= nums.length; i++) {
+            if (i == countBigger(nums,i)) {
+                return i;
             }
         }
 
-        return start;
+        return -1;
     }
 
     public static int countBigger (int[] nums, int target) {
@@ -27,17 +20,19 @@ public class Special_Array_With_X_Elements_Greater_Than_Or_Equal_X_1608 {
         while (start <= end) {
             int mid = start + (end -start) / 2;
 
-            if (nums[mid] > target) {
+            if (nums[mid] >= target) {
                 end = mid - 1;
             } else {
                 start = mid + 1;
             }
         }
 
-        return nums.length - end;
+        return nums.length - start;
     }
 
     public static void main(String[] args) {
-
+        System.out.println(specialArray(new int[] {3,5}));
+        System.out.println(specialArray(new int[] {0,0}));
+        System.out.println(specialArray(new int[] {0,4,3,0,4}));
     }
 }
