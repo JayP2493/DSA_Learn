ba1acf3c156db5bc3f595b4d4c2b7d7c376c69e8
jay1patel1 <jayp.fsd@outlook.com>
2024-09-20 13:33:12 +0530

    744 updated

--- 06_Searching/src/Find_Smallest_Letter_Greater_Than_Target_744.java
+++ 06_Searching/src/Find_Smallest_Letter_Greater_Than_Target_744.java
@@ -9,6 +9,23 @@ public class Find_Smallest_Letter_Greater_Than_Target_744 {
     public static char nextGreatestLetter(char[] letters, char target) {
         int start = 0;
         int end = letters.length-1;
+
+        while (start <= end) {
+            int mid = start + (end-start) / 2;
+
+            if (letters[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return letters[start % (letters.length)];
+    }
+
+    public static char nextGreatestLetterOld(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length-1;
         int targetChar = (target - 'a') + 1;
         int lastChar = (letters[letters.length-1] - 'a') + 1;
 
