11df9f040ff36d4964c762474adb234ceae4b8cd
jay1patel1 <jayp.fsd@outlook.com>
2024-09-30 20:56:11 +0530

    754 done

--- 06_Searching/src/Minimum_Absolute_Sum_Difference_1818.java
+++ 06_Searching/src/Minimum_Absolute_Sum_Difference_1818.java
@@ -11,6 +11,41 @@ public class Minimum_Absolute_Sum_Difference_1818 {
         }
 
         Arrays.sort(nums1);
+
+        int[] lowestDiff = new int[nums2.length];
+        int maxDiff = 0;
+
+        for (int i = 0; i < nums2.length; i++) {
+            int temp = lowerBound(nums2[i], nums1);
+            int tempDiff = Math.abs(nums2[i] - temp);
+
+            if (tempDiff < initialAbsoluteDiff[i]) {
+                maxDiff = Math.abs(tempDiff-initialAbsoluteDiff[i]);
+            }
+        }
+
+        return initialTotalSum - maxDiff;
+    }
+
+    public static int lowerBound (int target, int[] arr) {
+        int start = 0;
+        int end = arr.length-1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return arr[mid];
+            } else if (arr[mid] > target) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        int ans = Math.min()
+
+        return arr[end];
     }
 
     public static void main(String[] args) {
