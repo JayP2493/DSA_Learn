991f8a80384cdb4035389b28a1b997f65ba59fbe
jay1patel1 <jayp.fsd@outlook.com>
2024-10-06 20:12:01 +0530

    updated

--- 06_Searching/src/Median_Of_Two_Sorted_Arrays_4.js
+++ 06_Searching/src/Median_Of_Two_Sorted_Arrays_4.js
@@ -1,31 +1,35 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
 const findMedianSortedArrays = function(nums1, nums2) {
     if (nums1.length > nums2.length) {
-        return findMedianSortedArrays(nums2,nums1);
+        return findMedianSortedArrays (nums2, nums1);
     }
 
-    let n = nums1.length;
-    let m = nums2.length;
+    const m = nums1.length;
+    const n = nums2.length;
 
-    let ansLength = m+n;
-    let leftIndex = (m+m+1)/2;
+    const ansLength = m+n;
+    const ansIndex = Math.floor((ansLength+1)/2);
 
     let start = 0;
-    let end = nums1.length-1;
+    let end = m;
 
     while (start <= end) {
-        let mid = Math.floor(start + (end-start)/2);
-        let mid2 = leftIndex - mid;
+        let mid = Math.floor((start + end)/2);
+        let mid2 = ansIndex - mid;
 
         let l1 = Number.MIN_SAFE_INTEGER;
         let l2 = Number.MIN_SAFE_INTEGER;
-
         let r1 = Number.MAX_SAFE_INTEGER;
         let r2 = Number.MAX_SAFE_INTEGER;
 
-        if (mid < m) r1 = nums1[mid];
-        if (mid2 < n) r2 = nums2[mid2];
-        if (mid >= 0) l1 = nums1[mid-1];
-        if (mid2 >= 0) l2 = nums2[mid2-1];
+        if (m > mid) r1 = nums1[mid];
+        if (n > mid2) r2 = nums2[mid2];
+        if (0 <= mid-1) l1 = nums1[mid-1];
+        if (0 <= mid2-1) l2 = nums2[mid2-1];
 
         if (l1 <= r2 && l2 <= r1) {
             if (ansLength%2 === 1) {
@@ -33,7 +37,7 @@ const findMedianSortedArrays = function(nums1, nums2) {
             } else {
                 return (Math.max(l1,l2) + Math.min(r1,r2))/2;
             }
-        } else if (l1 > l2) {
+        } else if (l1 > r2) {
             end = mid - 1;
         } else {
             start = mid + 1;
@@ -42,3 +46,6 @@ const findMedianSortedArrays = function(nums1, nums2) {
 
     return 0;
 };
+
+
+findMedianSortedArrays([1,3],[2]);
