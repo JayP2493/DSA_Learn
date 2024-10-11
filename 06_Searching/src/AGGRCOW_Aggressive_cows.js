213c3b01d70d1b5e32e0824f3ade201484f72ed2
jay1patel1 <jayp.fsd@outlook.com>
2024-10-11 22:00:57 +0530

    aggresive cow done done

--- 06_Searching/src/AGGRCOW_Aggressive_cows.js
+++ 06_Searching/src/AGGRCOW_Aggressive_cows.js
@@ -1,36 +1,36 @@
-function check(x, k, stalls) {
-    let lastCow = stalls[0];
-    let count = 1;
+function aggressiveCow (stoles,k) {
+    stoles.sort((a, b) => a-b);
 
-    for (let i = 1; i < stalls.length; i++) {
-        if ((stalls[i] - lastCow) >= x) {
-            count++;
-            lastCow = stalls[i];
-        }
-    }
-
-    return count >= k;
-}
-
-function aggressiveCows(stalls, k) {
-    stalls.sort((a, b) => a - b);
-
-    let start = 1;  // Start from 1, because 0 makes no sense in terms of minimum distance
-    let end = stalls[stalls.length - 1] - stalls[0];
+    let start = 1;
+    let end = stoles[stoles.length-1] - stoles[0];
     let ans = 1;
 
     while (start <= end) {
-        let mid = Math.floor(start + (end - start) / 2);
+        let mid = Math.floor(start + (end-start)/2);
 
-        if (check(mid, k, stalls)) {
+        if (find(stoles,k,mid)) {
             ans = mid;
             start = mid + 1;
         } else {
-            end = mid-1;
+            end = mid - 1;
         }
     }
 
-    return mid;  // Return start instead of ans
+    function find(stoles,k,mid) {
+        let lastPos = stoles[0];
+        let count = 1;
+
+        for (let i = 1; i < stoles.length; i++) {
+            if ((stoles[i] - lastPos) >= mid) {
+                count++;
+                lastPos = stoles[i];
+            }
+        }
+
+        return count >= k;
+    }
+
+    return ans;
 }
 
-module.exports.aggressiveCows = aggressiveCows;
+console.log(aggressiveCow([1,2,3],2));
